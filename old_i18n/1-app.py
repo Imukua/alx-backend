82ae8a741ee80f87c5e5e0094c7ab64dc737e0b0
Imukua <ianmukuaa@gmail.com>
2024-01-14 16:32:49 +0300

    deffermment

---
+++ old_i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+flask app using babel
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    config class
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# configure the flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """
+    index route
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
