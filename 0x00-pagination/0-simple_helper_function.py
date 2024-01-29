9ff8d0624e73114baee2e7b2780c207d2fdd2f57
Imukua <ianmukuaa@gmail.com>
2024-01-29 13:59:57 +0300

    fixed indexing calc

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,8 +2,7 @@
 """script to return pagination parameters"""
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple[int, int]:
     startIdx = (page - 1) * page_size
-    endIdx = startIdx + page_size
-
+    endIdx = page * page_size
     return (startIdx, endIdx)
