d47fd0e62e14fd38bad72f6c2870ba80fe708535
Imukua <ianmukuaa@gmail.com>
2024-01-29 13:43:15 +0300

    Add simple helper function for pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""script to return pagination parameters"""
+
+def index_range(page, page_size):
+    startIdx = page - 1
+    endIdx = page_size
+
+    return tuple(startIdx, endIdx)
