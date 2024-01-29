48e41e93f3476a422a61f4ee18fb161eaad125b2
Imukua <ianmukuaa@gmail.com>
2024-01-29 14:06:13 +0300

    try

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 """script to return pagination parameters"""
-from typing import  Tuple
+
+
+from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    returns start and end index
+    """
+
     startIdx = (page - 1) * page_size
     endIdx = page * page_size
     return startIdx, endIdx
