8464746bfa7683e77e8a9c5997f56cea6a199fbd
Imukua <ianmukuaa@gmail.com>
2024-01-29 14:03:29 +0300

    fixed typing

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """script to return pagination parameters"""
-from typing import Tuple
+from typing import  Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     startIdx = (page - 1) * page_size
     endIdx = page * page_size
     return startIdx, endIdx
