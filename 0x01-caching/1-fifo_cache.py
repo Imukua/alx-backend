38d5392819ed2e91260a9d81df9488467e4d643f
Imukua <ianmukuaa@gmail.com>
2024-01-31 10:29:24 +0300

    implemented FIFO caching, hopefully

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+FIFO caching script
+"""
+
+
+BaseCaching = __import__("base_caaching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO caching implementation"""
+
+    def __init__(self):
+        """Initialisation"""
+
+        super().__init__()
+
+    def put(self, key, item):
+        """Stores item to the cache"""
+
+        if item is not None and key is not None:
+            if (
+                len(self.cache_data) > BaseCaching.MAX_ITEMS
+                and key not in self.cache_data.keys()
+            ):
+                firstIn = next(iter(self.cache_data.keys()))
+                del self.cache_data[firstIn]
+                print(f"DISCARD: {firstIn}")
+
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """ "Returns cached item given key"""
+        return self.cache_data.get(key)
