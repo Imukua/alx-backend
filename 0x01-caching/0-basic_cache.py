a832ed08b65547e7197b8d1d9b45887142d88c5b
Imukua <ianmukuaa@gmail.com>
2024-01-31 09:46:03 +0300

    implemented put method

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+implements put and get caching methods
+."""
+
+
+BaseCaching = __import__("BaseCaching").BaseCaching
+
+
+class BaseCache(BaseCaching):
+    """
+    Base cache class
+    """
+
+    def __init__(self):
+        """Initialisation"""
+
+        super().__init__()
+
+    def put(self, key, item):
+        """Puts items into the cache."""
+
+        if key is not None and item is not None:
+            self.cache_data[key] = item
