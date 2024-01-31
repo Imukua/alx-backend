3bfb85988bfb1b1eb212e3b0a96728eed65df26c
Imukua <ianmukuaa@gmail.com>
2024-02-01 00:15:58 +0300

    done

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+cache class base
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    cache class base
+    """
+
+    def __init__(self):
+        """
+        initialisation
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        put method
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                # delete the last item in the dictionary
+                lastkey, lastval = self.cache_data.popitem()
+                print("DISCARD: {}". format(lastkey))
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        get method
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+cache class base
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    caching
+    """
+
+    def __init__(self):
+        """
+        initialisation
+        """
+        super().__init__()
+        self.usedKeys = []
+
+    def put(self, key, item):
+        """
+        put method
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
+            else:
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+                d = self.usedKeys.pop(-2)
+                del self.cache_data[d]
+                print('DISCARD: {:s}'.format(d))
+
+    def get(self, key):
+        """
+        get method
+        """
+        if key is not None and key in self.cache_data.keys():
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
+        return None
