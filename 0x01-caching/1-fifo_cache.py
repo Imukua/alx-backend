5700dd9d0c807ad4d89a711954ada3fbed583fb8
Imukua <ianmukuaa@gmail.com>
2024-01-31 10:47:29 +0300

    updated get method

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -31,6 +31,6 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         """ "Returns cached item given key"""
-        if key is None or key not in self.cache_data:
+        if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data.get(key)
